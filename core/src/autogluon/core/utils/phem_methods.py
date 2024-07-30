0d9c84a67a04c6f5100c5b3cfff1f4b71e7f10c6
Ubuntu <ubuntu@ip-172-31-59-134.us-west-2.compute.internal>
2024-07-30 23:30:57 +0000

    Removing unecessary EnsembleSelectionMethod class

--- core/src/autogluon/core/utils/phem_methods.py
+++ core/src/autogluon/core/utils/phem_methods.py
@@ -11,8 +11,6 @@ from phem.application_utils.supported_metrics import msc
 from phem.base_utils.metrics import make_metric
 from sklearn.metrics import mean_squared_error
 
-from phem.methods.baselines import SingleBest
-from ...core.models.greedy_ensemble.ensemble_selection import EnsembleSelection
 from phem.methods.ensemble_weighting import CMAES
 from phem.methods.ensemble_selection.qdo import (
     QDOEnsembleSelection,
@@ -20,102 +18,7 @@ from phem.methods.ensemble_selection.qdo import (
 )
 from phem.examples.simulate_with_existing_data_example.simulate_based_on_sklearn_data import FakedFittedAndValidatedClassificationBaseModel
 
-
-
 logger = logging.getLogger(__name__)
-
-
-class EnsembleSelectionMethod(EnsembleSelection):
-    def __init__(
-        self,
-        ensemble_size: int,
-        problem_type: str,
-        metric,
-        sorted_initialization: bool = False,
-        bagging: bool = False,
-        tie_breaker: str = "random",
-        subsample_size: int | None = None,
-        random_state: np.random.RandomState = None,
-        **kwargs,
-    ) -> None:
-        
-        super().__init__(
-            ensemble_size=ensemble_size,
-            problem_type=problem_type,
-            metric=metric,
-            sorted_initialization=sorted_initialization,
-            bagging=bagging,
-            tie_breaker=tie_breaker,
-            subsample_size=subsample_size,
-            random_state=random_state,
-            **kwargs,
-        )
-        self.method_name = kwargs.get("ensemble_selection_method")
-          
-    def _fit(self, predictions: List[np.ndarray], labels: np.ndarray, time_limit=None, sample_weight=None):
-
-        base_models_data = base_models_creation(predictions)
-
-        score_metric = score_metric_creation(self.metric.name, labels)
-
-        
-        if (self.method_name == "greedy_ensemble_selection"):
-            es = EnsembleSelection(
-                ensemble_size=self.ensemble_size,
-                problem_type=self.problem_type,
-                metric=self.metric
-            )
-        elif (self.method_name == "cmaes" or self.method_name == "single_best"):
-            es = CMAES(
-                base_models=base_models_data,
-                n_iterations=self.ensemble_size,  # Number of iterations determined by self.ensemble_size
-                score_metric=score_metric,
-                random_state=1,
-            )
-        elif (self.method_name == "cmaes_with_normalization"):
-            es = CMAES(
-                base_models=base_models_data,
-                n_iterations=self.ensemble_size, # Number of iterations determined by self.ensemble_size
-                score_metric=score_metric,
-                random_state=1,
-                normalize_weights="softmax",
-                trim_weights="ges-like",
-            )
-        elif (self.method_name == "quality_optimization"):
-            es = QDOEnsembleSelection(
-                base_models=base_models_data,
-                n_iterations=self.ensemble_size, # Number of iterations determined by self.ensemble_size
-                archive_type="quality",
-                score_metric=score_metric,
-                random_state=1,
-            )
-        elif (self.method_name == "quality_diversity_optimization"):
-            es = QDOEnsembleSelection(
-                base_models=base_models_data,
-                n_iterations=self.ensemble_size, # Number of iterations determined by self.ensemble_size
-                score_metric=score_metric,
-                behavior_space=get_bs_ensemble_size_and_loss_correlation(),
-                random_state=1,
-            )
-
-        # Fit the ensemble on the validation data
-        if (self.method_name == "greedy_ensemble_selection"):
-            es.fit(predictions, labels)
-        else:
-            es.fit(predictions.T, labels)
-
-        #self.weights_ = es.weights_
-
-        if (self.method_name == "single_best"):
-            self.weights_ = es.single_best_stats[1]
-        else:
-            self.weights_ = es.weights_
-
-        return self.weights_
-
-    def _calculate_weights(self):
-        pass
-
 
 def base_models_creation(predictions: List[np.ndarray]):
 
